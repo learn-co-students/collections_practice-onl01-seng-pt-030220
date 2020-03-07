5347c52a98217deefabb78803fe1e097d260befa
Celeste Thomas <celestesmthomas@gmail.com>
2020-03-07 00:51:11 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
+# create a method "sort_array_asc" that takes a array of numbers(integers) and then sorts it from lowest to highest order
+
+def sort_array_asc(array)
+  array = array.sort!
+return array
+end
+
+# create a method "sort_array_desc" that takes an array of numbers and then sorts them from highest to lowest
+
+def sort_array_desc(array)
+  array = array.sort.reverse
+return array
+end
+
+# create a method "sort_array_char_count"
+def sort_array_char_count(array)
+  array = 
+
 
