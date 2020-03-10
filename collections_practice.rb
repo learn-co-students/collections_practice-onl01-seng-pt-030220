b8f2eb2327e1abf8f49ae721dfec990a506bee3c
Megan Christine McClure <mcclme02@luther.edu>
2020-03-10 18:27:28 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,8 +15,9 @@ end
    array 
 end 
 
-# def swap_elements_from_to(array, index, destination_index)
-  
+# def swap_elements_from_to(array, index, new_index)
+#   array[index], array[new_index] = array[new_index], array[index]
+#   return array 
 # end
 
 def reverse_array(array)
