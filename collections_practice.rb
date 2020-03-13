9faf5570519f1da8d81296ec9eef7243db37a7b4
James Dodds <dodds1990@gmail.com>
2020-03-13 02:10:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,83 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  temp = array[2]
+  array[2] = array[1]
+  array[1] = temp
+  array
+end
+
+def swap_elements3(array, index, destination_index)
+  temp = array[destination_index]
+  array[destination_index] = array[index]
+  array[index] = temp
+  array
+end
+
+def reverse_array(array)
+  reversed_array = []
+  array.each do |item|
+    reversed_array.unshift(item)
+  end
+  reversed_array
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+  array
+end
+
+def find_a(array)
+  new_array = array.select do |string|
+    string.start_with?("a")
+  end
+  new_array
+end
+
+def sum_array(array)
+  sum = array.inject do |sum, num|
+    sum += num
+  end
+  sum
+end
+
+def add_s(array)
+  new_array = array.each_with_index.collect do |element, index|
+    if index != 1
+      element << "s"
+    else
+      element
+    end
+  end
+  new_array
+end
