7fd20a1171ee1f7b5bd27a81e016621ffe021f75
celestesmthomas <celestesmthomas@gmail.com>
2020-04-25 16:18:23 -0700

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,97 @@
-# create a method "sort_array_asc" that takes a array of numbers(integers) and then sorts it from lowest to highest order
+# create a method "sort_array_asc" that takes a array of numbers(integers) 
+# and then sorts it from lowest to highest order
+require 'pry'
+def sort_array_asc(array)
+  return array.sort!
+end
 
 
+def sort_array_desc(array)
+  return array.sort.reverse!
+end
+
+
+def sort_array_char_count (array)
+  #binding.pry
+  array.sort_by do |word| 
+    word.length
+  end
+  end
+
+
+  def swap_elements(array)
+    #binding.pry
+    array[1], array[2] = array[2], array[1]
+    array
+  end
+
+
+  def reverse_array(array)
+    array.reverse!
+  end
+
+  def kesha_maker(array)
+    array.each do |ch|
+      ch[2] = "$"
+    end
+    array
+  end
+
+
+  def find_a(array)
+   array.find_all do |word|
+   word[0] == "a"
+     end
+  end
+
+  def sum_array(array)
+    array.inject do |sum, num|
+      sum + num
+    end
+  end
+
+  def add_s(array)
+   counter = 0
+   array.each_with_index.collect do |word, i|
+    if array[i] != array[1]
+      array[i] = "#{word}s"
+      counter += 1
+    end
+   end
+   array
+    end
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 def sort_array_asc(array)
   array = array.sort!
 return array
@@ -15,7 +106,7 @@ end
 
 # create a method "sort_array_char_count" sorting the array by the length of each value from lowest to highest
 def sort_array_char_count(array)
-  array = array.sort_by { |l| l.length }
+  array = array.sort_by { |num| num.length }
 end
 
 
@@ -65,4 +156,6 @@ def add_s(array)
 end
 return array
 end
+=end
+
 
